a5c8206a73598d1a6d8d941a804550fdc105df97
Tomás Ventura <126104459+TomasVentura17@users.noreply.github.com>
2023-06-27 18:24:39 +0000

    avance en creación de CRUD

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { VistaAdmin } from "./pages/vistaAdmin";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,7 +26,7 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
+                        <Route element={<VistaAdmin />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
---
+++ src/front/js/pages/vistaAdmin.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+import "../../styles/home.css";
+
+export const VistaAdmin = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		
+	);
+};
