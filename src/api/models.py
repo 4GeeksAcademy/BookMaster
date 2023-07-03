bf09995e314d28d6ea46b4b208a184a9519dc9ed
Stalin Narváez <108230261+StalinNarvaez@users.noreply.github.com>
2023-07-03 16:15:28 +0000

    avance carrito back

--- src/api/models.py
+++ src/api/models.py
@@ -2,21 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
+
 class Libro(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(100), nullable=False)
@@ -24,13 +26,15 @@ class Libro(db.Model):
     categoria = db.Column(db.String(100), nullable=False)
     detalle = db.Column(db.Text, nullable=False)
     precio = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, titulo, autor, categoria, detalle, precio):
+    def __init__(self, titulo, autor, categoria, detalle, precio, quantity):
         self.titulo = titulo
         self.autor = autor
         self.categoria = categoria
         self.detalle = detalle
         self.precio = precio
+        self.quantity = quantity
 
     def serialize(self):
         return {
@@ -39,8 +43,29 @@ class Libro(db.Model):
             "autor": self.autor,
             "categoria": self.categoria,
             "detalle": self.detalle,
-            "precio": self.precio
+            "precio": self.precio,
+            "quantity": self.quantity
         }
-    
 
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    libro_id = db.Column(db.Integer, db.ForeignKey('libro.id'), nullable=False)
+    libro = db.relationship('Libro', backref='cart_items')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref='cart_items')
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, libro, user, quantity):
+        self.libro = libro
+        self.user = user
+        self.quantity = quantity
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "libro": self.libro.serialize(),
+            "user": self.user.serialize(),
+            "quantity": self.quantity
+        }
     
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,12 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Libro
-from api.utils import generate_sitemap, APIException
-
+from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
+
+from .models import db, User, Libro, CartItem
+from .utils import generate_sitemap, APIException
 
 
 api = Blueprint('api', __name__)
@@ -15,12 +16,11 @@ api = Blueprint('api', __name__)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
     response_body = {
         "msg": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+
 
 @api.route('/signup', methods=['POST'])
 def handle_signup():
@@ -41,6 +41,7 @@ def handle_signup():
 
     return jsonify(response_body), 201
 
+
 @api.route('/login', methods=['POST'])
 def handle_login():
     auth = request.get_json()
@@ -59,10 +60,10 @@ def handle_login():
             "msg": "User does not exist. Check your credentials again or signup!"
         }
         return jsonify(response), 401
-    
+
     # check if the password is correct
     if user.password == auth.get("password"):
-        #create token with jwt
+        # create token with jwt
         access_token = create_access_token(identity=auth.get("email"))
         response = {
             "user": user.serialize(),
@@ -75,6 +76,7 @@ def handle_login():
         }
         return response, 403
 
+
 @api.route('/private', methods=['GET'])
 @jwt_required()
 def protected():
@@ -82,11 +84,13 @@ def protected():
     user_email = get_jwt_identity()
     return jsonify(logged_in_as=user_email), 200
 
+
 @api.route('/libros', methods=['GET'])
 def get_libros():
     libros = Libro.query.all()
     serialized_libros = [libro.serialize() for libro in libros]
     return jsonify(serialized_libros), 200
+
 
 @api.route('/libros', methods=['POST'])
 def create_libro():
@@ -96,11 +100,13 @@ def create_libro():
     detalle = request.json.get('detalle')
     precio = request.json.get('precio')
 
-    libro = Libro(titulo=titulo, autor=autor, categoria=categoria, detalle=detalle, precio=precio)
+    libro = Libro(titulo=titulo, autor=autor, categoria=categoria,
+                  detalle=detalle, precio=precio)
     db.session.add(libro)
     db.session.commit()
 
     return jsonify(libro.serialize()), 201
+
 
 @api.route('/libros/<int:libro_id>', methods=['GET'])
 def get_libro(libro_id):
@@ -108,21 +114,31 @@ def get_libro(libro_id):
     if libro:
         return jsonify(libro.serialize()), 200
     else:
-        return jsonify({'message': 'Libro not found'}), 404
+        return jsonify({"message": "Libro not found"}), 404
+
 
 @api.route('/libros/<int:libro_id>', methods=['PUT'])
 def update_libro(libro_id):
     libro = Libro.query.get(libro_id)
     if libro:
-        libro.titulo = request.json.get('titulo')
-        libro.autor = request.json.get('autor')
-        libro.categoria = request.json.get('categoria')
-        libro.detalle = request.json.get('detalle')
-        libro.precio = request.json.get('precio')
+        titulo = request.json.get('titulo')
+        autor = request.json.get('autor')
+        categoria = request.json.get('categoria')
+        detalle = request.json.get('detalle')
+        precio = request.json.get('precio')
+
+        libro.titulo = titulo
+        libro.autor = autor
+        libro.categoria = categoria
+        libro.detalle = detalle
+        libro.precio = precio
+
         db.session.commit()
+
         return jsonify(libro.serialize()), 200
     else:
-        return jsonify({'message': 'Libro not found'}), 404
+        return jsonify({"message": "Libro not found"}), 404
+
 
 @api.route('/libros/<int:libro_id>', methods=['DELETE'])
 def delete_libro(libro_id):
@@ -130,6 +146,63 @@ def delete_libro(libro_id):
     if libro:
         db.session.delete(libro)
         db.session.commit()
-        return jsonify({'message': 'Libro deleted'}), 200
+        return jsonify({"message": "Libro deleted"}), 200
     else:
-        return jsonify({'message': 'Libro not found'}), 404
+        return jsonify({"message": "Libro not found"}), 404
+
+#carrito de compras
+@api.route('/cart', methods=['GET'])
+def get_cart():
+    cart_items = CartItem.query.all()
+    serialized_cart_items = [item.serialize() for item in cart_items]
+    return jsonify(serialized_cart_items), 200
+
+
+@api.route('/cart', methods=['POST'])
+def add_to_cart():
+    libro_id = request.json.get('libro_id')
+    user_id = request.json.get('user_id')
+    quantity = request.json.get('quantity')
+
+    libro = Libro.query.get(libro_id)
+    user = User.query.get(user_id)
+
+    cart_item = CartItem(libro=libro, user=user, quantity=quantity)
+    db.session.add(cart_item)
+    db.session.commit()
+
+    return jsonify(cart_item.serialize()), 201
+
+
+@api.route('/cart/<int:cart_item_id>', methods=['DELETE'])
+def remove_from_cart(cart_item_id):
+    cart_item = CartItem.query.get(cart_item_id)
+    if cart_item:
+        db.session.delete(cart_item)
+        db.session.commit()
+        return jsonify({"message": "Cart item deleted"}), 200
+    else:
+        return jsonify({"message": "Cart item not found"}), 404
+
+
+@api.errorhandler(APIException)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+def create_app():
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(api)
+
+    return app
